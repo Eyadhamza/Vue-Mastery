66648914377fafe174fa5558ecf00252ffaa5256
Eyad Hamza <EyadHamza0@outlook.com>
2021-08-21 12:43:42 +0200

    getters and setters

--- src/store.js
+++ src/store.js
@@ -4,7 +4,33 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    'user': {'id':1234, 'name':'Eyad Hamza'},
+    'categories':[
+      'asda sda sd',
+      'asdasd',
+      'asda sda sd',
+      'asdasd',
+      'asda sda sd',
+      'asdasd',
+      'asda sda sd',
+      'asdasd',
+    ],
+    'events': [
+      {'id':1,'name':'Beyond Earth'},
+      {'id':2,'name':'Beyond Earth 1'},
+      {'id':3,'name':'Beyond Earth 23'},
+      {'id':4,'name':'Beyond Earth 33'},
+    ]
+  },
   mutations: {},
-  actions: {}
+  actions: {},
+  getters:{
+    catLength: state => {
+      return state.categories.length
+    },
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  }
 })
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,41 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create Event by {{user.name}}</h1>
+    <p>was created by {{user.id}}</p>
+
+    <p v-for="category in categories" :key="category">
+      {{category}}
+    </p>
+
+      {{ getEventById(1)}}
+
+
+    <p>number of categories = {{catLength}}</p>
+  </div>
+
 </template>
+
+
+<script>
+import {mapState,mapGetters} from 'vuex';
+
+export default {
+
+  computed:{
+    // instead of state => state.user.name, we use the shortcut
+    // this simply map state to a property
+    // and getters as well
+    // catLength(){
+    //   return this.$store.getters.catLength
+    // },
+    ...mapGetters(['getEventById']),
+    ...mapState(['user','categories'])
+  }
+
+}
+
+</script>
+
+<style>
+
+</style>
